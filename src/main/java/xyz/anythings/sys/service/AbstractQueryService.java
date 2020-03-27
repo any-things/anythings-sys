1694097d507f2d6306c2728b4d732803a16ae761
wryang <yang@yang-mac.local>
2020-03-27 10:37:49 +0900

    abstract 쿼리 매니저 수정
    1

--- src/main/java/xyz/anythings/sys/service/AbstractQueryService.java
+++ src/main/java/xyz/anythings/sys/service/AbstractQueryService.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import xyz.elidom.orm.IQueryManager;
+import xyz.elidom.orm.manager.DataSourceManager;
+import xyz.elidom.util.BeanUtil;
 
 /**
  * Anythings 기본 최상위 쿼리 서비스
@@ -21,4 +23,11 @@ public class AbstractQueryService {
 	 */
 	@Autowired
 	protected IQueryManager queryManager;
+	
+	/**
+	 * 엔티티에서 쿼리 매니저 가져오기 
+	 */
+	protected IQueryManager getDataSourceQueryManager(Class<?> entity) {
+		return BeanUtil.get(DataSourceManager.class).getQueryManager(entity);
+	}
 }
